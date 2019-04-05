b60e00ad15e8db317e4edfd2dd735ed71b63af3c
joody <joody@hubify.com.br>
2019-04-05 19:39:11 -0300

    Added the GameBoard.input() method
    The input takes the char and the position on the board and stores in the internal field map

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const gameBoard = new GameBoard();
 
 console.log('Tic Tac Toe game');
 console.log(gameBoard.getBoard());
+
+gameBoard.input('O', [0, 1]);
+console.log('Player 1 takes 0,1');
+console.log(gameBoard.getBoard());
+
+gameBoard.input('X', [1, 1]);
+console.log('Player 2 takes 1,1');
+console.log(gameBoard.getBoard());
--- tests/unit/board.js
+++ tests/unit/board.js
@@ -1,16 +1,35 @@
-/* global describe it expect */
+/* global describe it expect beforeEach */
 
 const GameBoard = require('../../lib/GameBoard');
 
-const gameBoard = new GameBoard();
+let gameBoard;
 
 describe('gameBoard.js', () => {
+  beforeEach(() => {
+    gameBoard = new GameBoard();
+  });
+
   describe('getBoard()', () => {
     it('displays the board', () => {
       const expectedOutput = `
-				| | | |
-				| | | |
-				| | | |
+|-|-|-|
+|-|-|-|
+|-|-|-|
+      `
+        .replace(/\t/g, '')
+        .trim();
+
+      expect(gameBoard.getBoard()).to.be.equals(expectedOutput);
+    });
+  });
+
+  describe('input', () => {
+    it('inputs a char on the board', () => {
+      gameBoard.input('O', [0, 0]);
+      const expectedOutput = `
+|O|-|-|
+|-|-|-|
+|-|-|-|
       `
         .replace(/\t/g, '')
         .trim();
